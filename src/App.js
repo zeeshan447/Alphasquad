3af469313825ee65ac3bee5fb25de58b9b47d1e3
zeeshan447 <zeeshananwar2011@gmail.com>
2021-08-05 20:29:24 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Home } from "./components/Home/Home";
 import { Navbar } from "./components/Layouts/Navbar";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { Pricing } from "./components/Pricing/Pricing";
 import { CaseStudy } from "./components/CaseStudies/CaseStudy";
 import { CaseStudyInner } from "./components/CaseStudiesInner/CaseStudyInner";
@@ -11,14 +11,12 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/pricing" component={Pricing} />
-          <Route path="/casestudies" component={CaseStudy} />
-          <Route path="/faq" component={CaseStudyInner} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/pricing" component={Pricing} />
+        <Route path="/casestudies" component={CaseStudy} />
+        <Route path="/faq" component={CaseStudyInner} />
+      </Switch>
       <Footer />
     </div>
   );
