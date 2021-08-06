ae5b96004a0f8755fda54352c818b7a93fc3ec83
zeeshan447 <zeeshananwar2011@gmail.com>
2021-08-06 19:31:04 +0500

    update

--- src/components/Pricing/GetResults.js
+++ src/components/Pricing/GetResults.js
@@ -7,10 +7,12 @@ export const GetResults = () => {
         <h1 className="text-center text-number_card text-white pt-4">
           Get Results, no matter what
         </h1>
-        <p className="text-center text-white">
+        <p className="text-center text-white mt-8 font-Mullish">
           Start your free trial today, you get all the pro features for 1 month
-          for you and your team. If you think its not for you can easily
-          unsubscribe
+          for you
+          <span className="block">
+            and your team. If you think its not for you can easily unsubscribe
+          </span>
         </p>
         <div className="flex flex-row justify-center items-center">
           <button className="font-Mullish mt-8 px-4 py-2 mb-8  w-40 h-14 text-cyan_dark bg-white">
